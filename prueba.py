2c3c89f739a81d1db7557ae0229e8d396c7ff4be
Nicolás R <cristianrodriguez3270@hotmail.com>
2023-01-24 02:26:58 -0500

    Libro de la tesis agregado

--- prueba.py
+++ prueba.py
@@ -1,24 +1,10 @@
-import multicarril as mp
-import sensibilidad_2_carriles as sen2
-import numpy as np
-import capacidad_NS as cap
-import main as mn
+from scipy.interpolate import lagrange, interp1d
+import math
 
+def interpolacion(x,y,z):
+    y_interp = interp1d(x, y,fill_value="extrapolate")
+    resultado = float(y_interp(z))
+    return round(resultado,3)
 
-datos = mn.Capacidad_Ns(3.65,1.30,7.5,1.30,55,82,50,10,40,763)
-res = cap.inter_compuesta_mon_esc(47.73,cap.tabla_9x,cap.montanoso,40,1.30)
-#sen2.sensiblidad_longitud(3.65,1.30,7.5,1.30,55,82,50,10,40,763,2000,"D")
- 
-datos_2 = cap.inter_compuesta3(cap.tabla_3x, cap.tabla_3,2,3.25)
-
-
-datos_3 = cap.inter_compuesta_plan_ond(60.40,cap.tabla_9x, cap.ondulado, 40, 3.78)
-
-
-datos3 = np.arange(0,1000,2)
-resultados = []
-for element in datos3:
-    var = cap.interpolacionp(cap.tabla_7x,cap.tabla_7,element/2070)
-    resultados.append(var)
-
-
+resultado = interpolacion([0.0,0.5,1.0,1.5,2.0,3.0],[2.80,1.60,1.30,0.90,0.70,0.0],1.3)
+print(resultado)
